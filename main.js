406413458a07aab88c0ecfb8f6061b88acdd8794
Campocruz <cosmacampo93@yahoo.it>
2026-03-23 19:23:23 +0100

    remove comment on second snack

--- main.js
+++ main.js
@@ -45,49 +45,49 @@ console.log(`La bicicletta meno pesante è: ${bikes[minorWeightKey].name} con un
 // Second SNACK
 
 // Create array object of teams
-// const teams = [
-//   {
-//     name: 'Juve',
-//     "points-scored": 0,
-//     "fouls-suffered": 0
-//   },
-//   {
-//     name: 'Milan',
-//     "points-scored": 0,
-//     "fouls-suffered": 0
-//   },
-//   {
-//     name: 'Inter',
-//     "points-scored": 0,
-//     "fouls-suffered": 0
-//   },
-//   {
-//     name: 'Atalanta',
-//     "points-scored": 0,
-//     "fouls-suffered": 0
-//   },
-//   {
-//     name: 'Bari',
-//     "points-scored": 0,
-//     "fouls-suffered": 0
-//   }
-// ];
-// // Empty array for insert the teams whit new value
-// const finalList = [];
-// // FOR IN cycle to change value for apecific key and after push in finalList arry 
-// for (let key in teams) {
-//   teams[key]["points-scored"] = randomNumber(1, 50);
-//   teams[key]["fouls-suffered"] = randomNumber(60, 100);
-//   finalList.push(`${teams[key].name} : ${teams[key]["fouls-suffered"]}`);
-// }
-// // Print the teams array with fouls suffered
-// for (let i = 0; i < finalList.length; i++) {
-//   console.log(finalList[i]);
-// }
+const teams = [
+  {
+    name: 'Juve',
+    "points-scored": 0,
+    "fouls-suffered": 0
+  },
+  {
+    name: 'Milan',
+    "points-scored": 0,
+    "fouls-suffered": 0
+  },
+  {
+    name: 'Inter',
+    "points-scored": 0,
+    "fouls-suffered": 0
+  },
+  {
+    name: 'Atalanta',
+    "points-scored": 0,
+    "fouls-suffered": 0
+  },
+  {
+    name: 'Bari',
+    "points-scored": 0,
+    "fouls-suffered": 0
+  }
+];
+// Empty array for insert the teams whit new value
+const finalList = [];
+// FOR IN cycle to change value for apecific key and after push in finalList arry 
+for (let key in teams) {
+  teams[key]["points-scored"] = randomNumber(1, 50);
+  teams[key]["fouls-suffered"] = randomNumber(60, 100);
+  finalList.push(`${teams[key].name} : ${teams[key]["fouls-suffered"]}`);
+}
+// Print the teams array with fouls suffered
+for (let i = 0; i < finalList.length; i++) {
+  console.log(finalList[i]);
+}
 
 
-// // Function declaretion 
-// function randomNumber(start, end) {
-//   const random_number = Math.floor((Math.random() * (end - start) + start));
-//   return random_number;
-// }
+// Function declaretion 
+function randomNumber(start, end) {
+  const random_number = Math.floor((Math.random() * (end - start) + start));
+  return random_number;
+}
